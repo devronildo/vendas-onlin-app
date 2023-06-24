af5335c0e43d7c25b8c9177dbaa6eb047b25a673
devronildo <ronildo_12@outlook.com>
2023-06-24 12:29:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from "react-native";
+
+const App = () => {
+    return (
+          <SafeAreaView>
+              <Text>Testando 1 2 s3</Text>
+          </SafeAreaView>
+    ) 
+}
+
+export default App;
